80c2b3260b1ab116380af7ccbca71e27de61869c
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-06-10 16:38:03 -0300

    [refactor, style] stilos dos botões das paginas de edição de tarefa e membro alterados. Comentarios e arquivos obsletos apagados

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,9 @@ Rails.application.routes.draw do
   get 'tarefas/new'
   get 'tarefas/create'
   get 'tarefa/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   get "/", to: "login#new"
-  #get "/login", to: "login#login"
   get "/home", to: "home#index"
-  #get "/cadastro", to: "cadastro#index"
   get "/cadastro", to: "cadastro#new"
   get "/cadastrotarefa", to: "cadastrotarefa#new"
   get "/listpage", to: "listpage#index"
@@ -34,8 +24,6 @@ Rails.application.routes.draw do
   patch 'edit/:id/update', to: 'edit#update', as: 'update_edit'
   patch 'edit/:id/updatedatetime', to: 'edit#updatedatetime', as: 'updatedatetime_edit'
   delete 'edit/:id', to: 'edit#destroy', as: 'destroy_edit'
-
-
 
   resources :edit, only: [:edit, :update], as: 'edit'
   resources :tarefas, only: [:new, :create]
